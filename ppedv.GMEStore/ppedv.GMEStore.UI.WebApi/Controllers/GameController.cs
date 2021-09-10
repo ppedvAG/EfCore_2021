76223a0293bf7d81653312e84276f3d8ce8dd2c3
André <AndreR@outlook.de>
2021-09-10 17:08:35 +0200

    WebAPI

---
+++ ppedv.GMEStore/ppedv.GMEStore.UI.WebApi/Controllers/GameController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ppedv.GMEStore.Logic;
+using ppedv.GMEStore.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ppedv.GMEStore.UI.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GameController : ControllerBase
+    {
+        Core core = new Core(null);
+
+        // GET: api/<GameController>
+        [HttpGet]
+        public IEnumerable<Game> Get()
+        {
+            return core.UnitOfWork.GameRepository.QueryGamesIncludingAll();
+        }
+
+        // GET api/<GameController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<GameController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<GameController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<GameController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
