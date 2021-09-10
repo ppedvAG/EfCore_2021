b4c89020952cb1ad3d93651dfdd1eacb9b9c388e
André <AndreR@outlook.de>
2021-09-10 11:24:09 +0200

    Logic gestartet, Model in Model Folder und Repo Contract definiert

---
+++ ppedv.GMEStore/ppedv.GMEStore.Logic/Core.cs
@@ -0,0 +1,14 @@
+﻿using ppedv.GMEStore.Model;
+using System;
+
+namespace ppedv.GMEStore.Logic
+{
+    public class Core
+    {
+
+        public Company GetCompanyThatPublisheMostGamesOfYear(int year)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
