6c3cf307df8975a4635db211ae3cfef7551de1d2
André <AndreR@outlook.de>
2021-09-10 13:23:23 +0200

    DI per Reflection

--- ppedv.GMEStore/ppedv.GMEStore.Data.EFCore/EfRepository.cs
+++ ppedv.GMEStore/ppedv.GMEStore.Data.EFCore/EfRepository.cs
@@ -1,11 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using ppedv.GMEStore.Model;
 using ppedv.GMEStore.Model.Contracts;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ppedv.GMEStore.Data.EFCore
 {
--- ppedv.GMEStore/ppedv.GMEStore.UI.DevConsole/Program.cs
+++ ppedv.GMEStore/ppedv.GMEStore.UI.DevConsole/Program.cs
@@ -1,7 +1,9 @@
 ﻿using ppedv.GMEStore.Logic;
 using ppedv.GMEStore.Model;
+using ppedv.GMEStore.Model.Contracts;
 using System;
 using System.Linq;
+using System.Reflection;
 
 namespace ppedv.GMEStore.UI.DevConsole
 {
@@ -11,7 +13,15 @@ namespace ppedv.GMEStore.UI.DevConsole
         {
             Console.WriteLine("Hello World!");
 
-            var core = new Core(new Data.EFCore.EfRepository());
+            //injection per Hand per Refection
+            var assPath = @"C:\Users\Fred\source\repos\EfCore_2021\ppedv.GMEStore\ppedv.GMEStore.Data.EFCore\bin\Debug\net5.0\ppedv.GMEStore.Data.EFCore.dll";
+            var ass = Assembly.LoadFrom(assPath);
+            var typeMitRepo = ass.GetTypes().FirstOrDefault(x => x.GetInterfaces().Contains(typeof(IRepository)));
+            var efRepoInst =  (IRepository)Activator.CreateInstance(typeMitRepo);
+            var core = new Core(efRepoInst); 
+
+            //injection per Hand und direkt
+            //var core = new Core(new Data.EFCore.EfRepository()); 
 
             //foreach (var g in core.Repository.Query<Game>().Where(x => x.Name.ToLower().Contains("e")).OrderBy(x => x.Published))
             foreach (var g in core.Repository.QueryGamesIncludingAll().Where(x => x.Name.ToLower().Contains("e")).OrderBy(x => x.Published))
